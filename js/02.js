7596a0cb2db86ca6d9dc72689f25dcae9591a6dd
Karan8501 <106527836+Karan8501@users.noreply.github.com>
2023-04-29 20:11:07 +0530

    Add files via upload

---
+++ js/02.js
@@ -0,0 +1,15 @@
+"use strict";
+// Intro to variable
+
+// variable can store some information 
+// we can change it later or accsess it
+
+// declration of variable
+
+var Name = "karan";
+
+// use a variable
+console.log(Name);
+// change variable
+var Name = "kishan";
+console.log(Name);
